7f2044e3e66095caf070df7b9aa664f7876ca2f8
Tomás G. <30012262+0x00cl@users.noreply.github.com>
2024-03-10 20:52:38 -0300

    ADD: YAMLs for deployment in Kubernetes cluster

--- deployment/main.tf
+++ deployment/main.tf
@@ -10,5 +10,12 @@ terraform {
 }
 
 provider "aws" {
+  alias  = "us_east_1"
   region = "us-east-1"
+}
+
+resource "aws_ecrpublic_repository" "devsu" {
+  provider = aws.us_east_1
+  repository_name = "postulacion-devsu"
+
 }
